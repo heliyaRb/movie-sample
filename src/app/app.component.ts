a04fb5b73115a8e1f390fea648d286e532e1ef63
Heliya Rahbar <zahrarahbar1991@gmail.com.com>
2021-11-18 17:15:24 +0330

    feat: :art: improove list component and api call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { MovieModel } from './core/models/movie.model';
+import { MovieService } from './core/services/movie.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'movie-list';
+  _movieList: MovieModel[] = [];
+  moviesListener$: Subscription | undefined;
+
+  get movieList() {
+    return this._movieList;
+  }
+
+  constructor(private movieService: MovieService) {
+    this.initialList();
+  }
+
+  ngOnInit() {}
+
+  initialList() {
+    this.moviesListener$ = this.movieService
+      .searchMovie('green')
+      .subscribe((result) => {
+        this._movieList = result;
+      });
+  }
+
+  ngOnDestroy() {
+    this.moviesListener$?.unsubscribe();
+  }
+
+  onCardClick(id: string) {
+    console.log(id);
+  }
 }
--- src/app/components/listing/listing.component.ts
+++ src/app/components/listing/listing.component.ts
@@ -1,14 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { MovieModel } from 'src/app/core/models/movie.model';
 
 @Component({
   selector: 'app-listing',
   templateUrl: './listing.component.html',
   styleUrls: ['./listing.component.scss'],
 })
-export class ListingComponent implements OnInit {
+export class ListingComponent {
   searchIcon = faSearch;
+  dataList: MovieModel[] = [];
+
+  @Input('listData') listData: MovieModel[] = [];
+  @Output('cardClick') cardClick = new EventEmitter<string>();
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnChanges() {
+    this.dataList = this.listData;
+  }
+
+  onCardCliced(id: string) {
+    this.cardClick.emit(id);
+  }
 }
