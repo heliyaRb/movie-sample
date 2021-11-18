93a11ee69eaa3cf1fde5bffc0de16bbd990060ed
Heliya Rahbar <zahrarahbar1991@gmail.com.com>
2021-11-18 20:47:48 +0330

    feat: :lipstick: improve detail template

--- src/app/components/detail/detail.component.ts
+++ src/app/components/detail/detail.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  faCalendar,
+  faClock,
+  faGlobe,
+  faLanguage,
+  faMoneyBill,
+  faStar,
+  faTrophy,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
-  styleUrls: ['./detail.component.scss']
+  styleUrls: ['./detail.component.scss'],
 })
 export class DetailComponent implements OnInit {
+  starIcon = faStar;
+  calendarIcon = faCalendar;
+  clockIcon = faClock;
+  languageIcon = faLanguage;
+  countryIcon = faGlobe;
+  awardIcon = faTrophy;
+  moneyIcon = faMoneyBill;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
