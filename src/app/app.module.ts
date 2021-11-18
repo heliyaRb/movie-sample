dd822a587c519efe5f2551f49d71008e8fd5bb14
Heliya Rahbar <zahrarahbar1991@gmail.com.com>
2021-11-18 16:39:23 +0330

    refactor: :art: update app component template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { DetailComponent } from './components/detail/detail.component';
+import { ListingComponent } from './components/listing/listing.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, DetailComponent, ListingComponent],
   imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
