ffb9e6df34d7d60eb99a18563ac71c6d0664c6b2
Heliya Rahbar <zahrarahbar1991@gmail.com.com>
2021-11-19 12:07:00 +0330

    feat: :art: improve detail component and bind data

--- src/app/components/detail/detail.component.ts
+++ src/app/components/detail/detail.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 import {
   faCalendar,
   faClock,
@@ -8,13 +14,16 @@ import {
   faStar,
   faTrophy,
 } from '@fortawesome/free-solid-svg-icons';
+import { Subscription } from 'rxjs';
+import { MovieDetailModel } from 'src/app/core/models/movie-detail.model';
+import { MovieDetailService } from 'src/app/core/services/movie-detail.service';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.scss'],
 })
-export class DetailComponent implements OnInit {
+export class DetailComponent implements OnInit, OnChanges {
   starIcon = faStar;
   calendarIcon = faCalendar;
   clockIcon = faClock;
@@ -22,7 +31,24 @@ export class DetailComponent implements OnInit {
   countryIcon = faGlobe;
   awardIcon = faTrophy;
   moneyIcon = faMoneyBill;
-  constructor() {}
+  movieData!: MovieDetailModel;
+  movieDetailListener$: Subscription | undefined;
+
+  @Input('imdbId') imdbId!: string;
+
+  constructor(private movieDetailService: MovieDetailService) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.movieDetailListener$ = this.movieDetailService
+      .getMovieDetail(this.imdbId ? this.imdbId : 'tt6966692 ')
+      .subscribe((result) => {
+        this.movieData = result;
+      });
+  }
 
   ngOnInit(): void {}
+
+  onDestroy() {
+    this.movieDetailListener$?.unsubscribe();
+  }
 }
